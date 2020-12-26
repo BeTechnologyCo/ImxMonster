ccb8dfc40e73ce27cea37253f1ccebb10c8700cc
AthulDilip <athul@fantacode.com>
2020-12-26 12:04:28 +0530

    Catching Pokemons

--- Assets/Scripts/Pokemons/Pokemon.cs
+++ Assets/Scripts/Pokemons/Pokemon.cs
@@ -9,6 +9,14 @@ public class Pokemon
     [SerializeField] PokemonBase _base;
     [SerializeField] int level;
 
+    public Pokemon(PokemonBase pBase, int pLevel)
+    {
+        _base = pBase;
+        level = pLevel;
+
+        Init();
+    }
+
     public PokemonBase Base { 
         get {
             return _base;
@@ -31,7 +39,7 @@ public class Pokemon
     public int VolatileStatusTime { get; set; }
 
 
-    public Queue<string> StatusChanges { get; private set; } = new Queue<string>();
+    public Queue<string> StatusChanges { get; private set; }
     public bool HpChanged { get; set; }
     public event System.Action OnStatusChanged;
 
@@ -51,6 +59,7 @@ public class Pokemon
         CalculateStats();
         HP = MaxHp;
 
+        StatusChanges = new Queue<string>();
         ResetStatBoost();
         Status = null;
         VolatileStatus = null;
