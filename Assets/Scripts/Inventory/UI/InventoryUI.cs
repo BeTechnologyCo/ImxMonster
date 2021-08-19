71e2738cc6a2165c3e40ddcdf3e480322b1ca610
Athul <athuldilip@outlook.com>
2021-08-19 10:17:30 +0530

    Using Pokeball Items from Inventory

--- Assets/Scripts/Inventory/UI/InventoryUI.cs
+++ Assets/Scripts/Inventory/UI/InventoryUI.cs
@@ -20,7 +20,7 @@ public class InventoryUI : MonoBehaviour
 
     [SerializeField] PartyScreen partyScreen;
 
-    Action onItemUsed;
+    Action<ItemBase> onItemUsed;
 
     int selectedItem = 0;
     int selectedCategory = 0;
@@ -63,7 +63,7 @@ public class InventoryUI : MonoBehaviour
         UpdateItemSelection();
     }
 
-    public void HandleUpdate(Action onBack, Action onItemUsed = null)
+    public void HandleUpdate(Action onBack, Action<ItemBase> onItemUsed = null)
     {
         this.onItemUsed = onItemUsed;
 
@@ -100,7 +100,7 @@ public class InventoryUI : MonoBehaviour
             }
 
             if (Input.GetKeyDown(KeyCode.Z))
-                OpenPartyScreen();
+                ItemSelected();
             else if (Input.GetKeyDown(KeyCode.X))
                 onBack?.Invoke();
         }
@@ -120,6 +120,18 @@ public class InventoryUI : MonoBehaviour
         }
     }
 
+    void ItemSelected()
+    {
+        if (selectedCategory == (int)ItemCategory.Pokeballs)
+        {
+            StartCoroutine(UseItem());
+        }
+        else
+        {
+            OpenPartyScreen();
+        }
+    }
+
     IEnumerator UseItem()
     {
         state = InventoryUIState.Busy;
@@ -127,8 +139,10 @@ public class InventoryUI : MonoBehaviour
         var usedItem = inventory.UseItem(selectedItem, partyScreen.SelectedMember, selectedCategory);
         if (usedItem != null)
         {
-            yield return DialogManager.Instance.ShowDialogText($"The player used {usedItem.Name}");
-            onItemUsed?.Invoke();
+            if (!(usedItem is PokeballItem))
+                yield return DialogManager.Instance.ShowDialogText($"The player used {usedItem.Name}");
+
+            onItemUsed?.Invoke(usedItem);
         }
         else
         {
