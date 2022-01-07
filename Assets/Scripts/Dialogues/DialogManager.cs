203230b87d4d4f69acd3ea9c57d52da9cb619b02
Athul <athuldilip@outlook.com>
2022-01-08 05:10:05 +0530

    Dialogue Choices

--- Assets/Scripts/Dialogues/DialogManager.cs
+++ Assets/Scripts/Dialogues/DialogManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class DialogManager : MonoBehaviour
 {
     [SerializeField] GameObject dialogBox;
+    [SerializeField] ChoiceBox choiceBox;
     [SerializeField] Text dialogText;
     [SerializeField] int lettersPerSecond;
 
@@ -46,7 +47,8 @@ public class DialogManager : MonoBehaviour
         IsShowing = false;
     }
 
-    public IEnumerator ShowDialog(Dialog dialog)
+    public IEnumerator ShowDialog(Dialog dialog, List<string> choices = null,
+        Action<int> onChoiceSelected = null)
     {
         yield return new WaitForEndOfFrame();
 
@@ -60,10 +62,16 @@ public class DialogManager : MonoBehaviour
             yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Z));
         }
 
+        if (choices != null && choices.Count > 1)
+        {
+            yield return choiceBox.ShowChoices(choices, onChoiceSelected);
+        }
+
         dialogBox.SetActive(false);
         IsShowing = false;
         OnDialogFinished?.Invoke();
     }
+
 
     public void HandleUpdate()
     {
